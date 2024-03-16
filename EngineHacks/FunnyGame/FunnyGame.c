031a5551e88dd8563e92de37009c3af8a9b814f3
Sme <esahig15789@gmail.com>
2024-03-16 16:12:36 -0400

    fung

--- EngineHacks/FunnyGame/FunnyGame.c
+++ EngineHacks/FunnyGame/FunnyGame.c
@@ -4,16 +4,42 @@ void GameStartASMC(Proc* parent) {
 	ProcStartBlocking(&ProcScr_FunnyGameMain, parent);
 }
 
-void FunnyGameInit(Proc* parent) {
+void FunnyGameInit(FunnyGameProc* parent) {
 	//init game
+	
+	//unlock basic rod
+	FunnyGameState->rods[ROD_1] = 1;
+	
+	//100 basic bait
+	FunnyGameState->baitCount[BAIT_1] = 100;
 	
 }
 
-void FunnyGameMainInit(Proc* parent) {
+void FunnyGameMainInit(FunnyGameProc* parent) {
 	//runs just before main loop, while faded to black
+	
+	//load the main game screen
+	
+	//generate 4 random fish
+	
+	//start the cast selection menu
+	
+	
 }
 
-void FunnyGameMainLoop(Proc* parent) {
+void FunnyGameMainLoop(FunnyGameProc* parent) {
 	//main loop
+	
+	//move fish as needed
+	
+	//wait for cast selection menu to not exist
+	
+		//switch on game state
+	
+			//case: casting line
+			
+			//case: line casted
+			
+			//case: fish caught or escaped
 	
 }
--- EngineHacks/FunnyGame/FunnyGame.h
+++ EngineHacks/FunnyGame/FunnyGame.h
@@ -1,6 +1,51 @@
 #include "gbafe.h"
 
+struct FishInfo {
+	u8 fishType;
+	u8 movementTimer;
+	u8 xPos;
+	u8 yPos;
+}
+
+struct FunnyGameProc {
+	PROC_FIELDS;
+	u8 gameState;
+	FishInfo activeFish[4];
+} typedef FunnyGameProc;
+
+struct BestiaryRecord {
+	u8 sizeRecord;
+	u8 numCaught;
+} typedef BestiaryRecord;
+
+struct GameStateInfo {
+	u8 rods[4];
+	u8 baitCount[4];
+	BestiaryRecord bestiaryRecords[256];
+} typedef GameStateInfo;
+
+struct BestiaryInfo {
+	short nameTextID;
+	short descTextID;
+	u16 minLength;
+	u16 maxLength;
+} typedef BestiaryInfo;
+
+struct 
+
+#define ROD_1 0
+#define ROD_2 1
+#define ROD_3 2
+#define ROD_4 3
+
+#define BAIT_1 0
+#define BAIT_2 1
+#define BAIT_3 2
+#define BAIT_4 3
+
 extern const ProcInstruction ProcScr_FunnyGameMain;
+extern const BestiaryInfo BestiaryTable[256];
+extern GameStateInfo* FunnyGameState;
 
 void GameStartASMC(Proc* parent);
 void FunnyGameInit(Proc* parent);
